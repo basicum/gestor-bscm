a79d0976f3509c43afa23c2600017cfdeab894ca
fer@basicum.com <fer@basicum.com>
2019-10-27 11:37:38 +0100

    creacion de archivos de la aplicación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <!-- Required meta tags -->
-
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>Administrador Basicum</title>
     <!-- plugins:css -->
@@ -513,7 +512,6 @@
             });
 
             todoListItem.on('click', '.remove', function() {
-
 
                 $(this).parent().remove();
             });
