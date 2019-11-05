befe5fcbfe2caf5acaf9a9b97abef22e8b8cddee
fer@basicum.com <fer@basicum.com>
2019-11-05 11:49:16 +0100

    actualizacion laravel 5.8

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,7 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout' );
 Route::auth();
 
 Route::get('/', 'HomeController@index')->name('home');
--- config/app.php
+++ config/app.php
@@ -155,7 +155,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Styde\Html\HtmlServiceProvider::class,
+        Laravel\Tinker\TinkerServiceProvider::class,
     ],
 
     /*
